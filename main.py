493d733965f778677deaefc3d70eb058dd25ee6d
paulAAV98 <59033526+paulAAV98@users.noreply.github.com>
2023-04-20 17:34:27 -0500

    Creando nuevas rutas con parametros
    Creando nuevas rutas con parametros de ruta y de consulta

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ from fastapi import FastAPI
 # Creacion de una aplicacion FastAPI:
 app = FastAPI()
 
+aplicacion = FastAPI()
+
 @app.get('/')
-def read_root():
+def read_mundo():
     return {'Hello': 'World!'}
 
-app.get('/hola')
+@app.get('/hola')
 def hola_mundo():
     return {'Hola' : 'Mundo'}
+
+@app.get('/items/{item_id}')
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {'item_id': item_id, 'q': q}
+
+
+@app.get('/calculadora/{numero_1}{numero_2}')
+def calculadora(numero_1: int, numero_2: int):
+    return {'resultado': numero_1 + numero_2}
