3da8bf03746ad3991029b14cd394fcbeef613f53
paulAAV98 <59033526+paulAAV98@users.noreply.github.com>
2023-04-23 19:46:44 -0500

    Creada la ruta PUT para actualizar un item
    Creada la ruta PUT para actualizar un item

--- main.py
+++ main.py
@@ -27,11 +27,10 @@ def read_item(item_id: int, q: Union[str, None] = None):
 # = None significa opcional
     return {'item_id': item_id, 'q': q}
 
-
 @app.get('/calculadora/{numero_1}{numero_2}')
 def calculadora(numero_1: int, numero_2: int):
     return {'resultado': numero_1 + numero_2}
 
 @app.put('/items/{item_id}')
 def update_item(item_id: int, item: Item):
-    return {'item_name': item.name, 'item_id': item_id}
+    return {'item_name': item.name, 'item_id': item_id, 'item_price': item.price}
